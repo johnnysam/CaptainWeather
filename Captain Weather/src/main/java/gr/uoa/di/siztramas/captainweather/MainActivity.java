636bd09df1231330fd11ac826df07508d6245445
Ioannis Samartzis <ioannis.samartzis@gmail.com>
2016-06-03 11:13:30 +0200

    Launch Icon change
    + imports optimization

--- Captain Weather/src/main/java/gr/uoa/di/siztramas/captainweather/MainActivity.java
+++ Captain Weather/src/main/java/gr/uoa/di/siztramas/captainweather/MainActivity.java
@@ -1,14 +1,9 @@
 package gr.uoa.di.siztramas.captainweather;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements MainFragment.OnFragmentInteractionListener {
     private static final String TAG = "MAIN_ACTIVITY";
@@ -18,21 +13,6 @@ public class MainActivity extends AppCompatActivity implements MainFragment.OnFr
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(TAG, "onCreate()");
-
-       /* FragmentManager mFragmentManager = getFragmentManager();
-        FragmentTransaction mfragmentTransaction = mFragmentManager.beginTransaction();
-
-        // Create new fragment and transaction
-        Fragment Fragment = new Fragment();
-        FragmentTransaction transaction = getFragmentManager().beginTransaction();
-
-        // Replace whatever is in the fragment_container view with this fragment,
-        // and add the transaction to the back stack
-        transaction.replace(R.id.fragment_container, newFragment);
-        transaction.addToBackStack(null);
-
-        // Commit the transaction
-        transaction.commit();*/
     }
 
     @Override
