2859fdd423ba66752b10c642aee392cb79ecab64
Ioannis Samartzis <ioannis.samartzis@gmail.com>
2016-06-07 14:56:54 +0200

    Details activity
    listView item functionality added to initiate a new detail activity when
    clicked. DetailActivity for now shows just a string with the forecast.

---
+++ Captain Weather/src/main/java/gr/uoa/di/siztramas/captainweather/DetailActivity.java
@@ -0,0 +1,84 @@
+package gr.uoa.di.siztramas.captainweather;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private final String TAG = "DETAIL_ACTIVITY";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        Log.d(TAG, "onCreate()");
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new DetailFragment())
+                    .commit();
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy()");
+        super.onDestroy();
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class DetailFragment extends Fragment {
+
+        public DetailFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+            // The detail Activity called via intent.  Inspect the intent for forecast data.
+            Intent intent = getActivity().getIntent();
+            if (intent != null && intent.hasExtra("Forecast")) {
+                String forecastStr = intent.getStringExtra("Forecast");
+                ((TextView) rootView.findViewById(R.id.detail_tv)).setText(forecastStr);
+            }
+
+            return rootView;
+        }
+    }
+}
--- Captain Weather/src/main/java/gr/uoa/di/siztramas/captainweather/ForecastFragment.java
+++ Captain Weather/src/main/java/gr/uoa/di/siztramas/captainweather/ForecastFragment.java
@@ -1,6 +1,7 @@
 package gr.uoa.di.siztramas.captainweather;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -115,8 +117,16 @@ public class ForecastFragment extends Fragment {
         // Get a reference to the ListView, and attach this adapter to it.
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
-        Log.d(TAG, "ListView Adapter attached");
-
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String forecast = mForecastAdapter.getItem(position);
+                Intent detailIntent = new Intent(getActivity(), DetailActivity.class)
+                        .putExtra("Forecast", forecast);
+                Log.d(TAG, forecast);
+                startActivity(detailIntent);
+            }
+        });
         return rootView;
     }
 
